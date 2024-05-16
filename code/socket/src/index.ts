c48fc1575510a60c673cb7a7ff8dff5257900156
MrDrummer <mrdrummer.programming@hotmail.com>
2024-05-16 18:11:18 +0100

    Add build scripts to packages

--- code/socket/src/index.ts
+++ code/socket/src/index.ts
@@ -1,6 +1,20 @@
 import { ObsHook } from "@obs-hook/core"
-import { Config } from "@obs-hook/models"
+// import { Config } from "@obs-hook/models"
 
-const hook = new ObsHook()
+const hook = new ObsHook({
+  cut: 'cut',
+  audio: [
+    { slug: 'audio1', desc: 'audio1', source: 'source1' }
+  ],
+  scenes: [
+    { slug: 'scene1', desc: 'scene1', scene: 'scene1' }
+  ],
+  layouts: [
+    { slug: 'layout1', desc: 'layout1', scene: 'scene1', slots: ['slot1'] }
+  ],
+  slots: [
+    { slug: 'slot1', desc: 'slot1', scene: 'scene1', scenes: ['scene1'] }
+  ]
+})
 
-hook.doThing()
+// hook.doThing()
